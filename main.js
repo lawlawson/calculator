ca188594d3c7d60535ae1a9a1081de91196978c6
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-04-02 16:21:45 +0100

    Fixed input with operators

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ for (let key of keys) {
       display_output.innerHTML = '';
     } else if (value == 'backspace') {
       input = input.input.slice(0, -1);
-      display_input.innerHTML = input;
+      display_input.innerHTML = CleanInput(input);
     } else if (value == '=') {
       let result = eval(input);
 
@@ -35,10 +35,35 @@ for (let key of keys) {
       ) {
         input += ')';
       }
-      display_input.innerHTML = input;
+      display_input.innerHTML = CleanInput(input);
     } else {
       input += value;
-      display_input.innerHTML = input;
+      display_input.innerHTML = CleanInput(input);
     }
   });
+}
+
+function CleanInput(input) {
+  let input_array = input.split('');
+  let input_array_length = input_array.length;
+
+  for (let i = 0; i < input_array_length; i++) {
+    if (input_array[i] == '*') {
+      input_array[i] = `<span class="operator">x</span>`;
+    } else if (input_array[i] == '/') {
+      input_array[i] = `<span class="operator">÷</span>`;
+    } else if (input_array[i] == '+') {
+      input_array[i] = `<span class="operator">+</span>`;
+    } else if (input_array[i] == '-') {
+      input_array[i] = `<span class="operator">-</span>`;
+    } else if (input_array[i] == '(') {
+      input_array[i] = `<span class="brackets">(</span>`;
+    } else if (input_array[i] == ')') {
+      input_array[i] = `<span class="brackets">)</span>`;
+    } else if (input_array[i] == '%') {
+      input_array[i] = `<span class="percent">%</span>`;
+    }
+  }
+
+  return input_array.join('');
 }
